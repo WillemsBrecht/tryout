2b8fd5cffbe6dedb5b69e91248142b12efc9e09f
WillemsBrecht <brecht.willems2@student.howest.be>
2018-02-26 12:16:16 +0100

    Add project files.

---
+++ tryout/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tryout.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
